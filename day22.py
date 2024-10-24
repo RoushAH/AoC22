edad2e10088ebe47ecf695db3657ef38b87eb9c8
RoushAH <Roush.Adam.H@gmail.com>
2024-10-24 20:32:48 +0100

    Continued ingestion

--- day22.py
+++ day22.py
@@ -1,6 +1,15 @@
+from collections import deque
+from listfuncs import merge
+
 TESTING = True
 R = "R"
 L = "L"
+N = (0,-1)
+S = (0,1)
+E = (1,0)
+W = (-1,0)
+dirs = deque([E,S,W,N])
+turns = {R:-1, L:1}
 
 filename = "day22sample.txt" if TESTING else "day22.txt"
 
@@ -10,5 +19,8 @@ if __name__ == "__main__":
     # ingest data, steps first 'cause they're easier
     steps = data[-1].replace(R, " R ").replace(L, " L ")
     steps = steps.split()
-
+    for i in range(len(steps)):
+        if i % 2 == 0:
+            steps[i] = int(steps[i])
     print(steps)
+    # Not sure if I need to pad for movement
