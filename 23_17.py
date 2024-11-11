6ace2bee5775a79eadb11dac3806d12f694bdebf
RoushAH <Roush.Adam.H@gmail.com>
2024-11-11 19:57:19 +0000

    started 23_17

--- 23_17.py
+++ 23_17.py
@@ -1,10 +1,25 @@
 # Iterate down the major diagonal to find the minimum bid
-# Go recursive to explore for better  --
-# # Fail case is if current path is already > min bid
-# # Else check L, R, and C (if it's >= 3 Cs in a row)
-# # Recursive call is  ((x,y), loss_so_far, C-streak)
+# Dijkstra-man!
+# Do Dijkstra for each cel, treating each as a little node
+# Perhaps replace the 2d grid with a grid of node objects?
+# Each having edge detection ability, a heat value, and a "previous".
+# On discovery, store a node, then check to see which nodes you can legally reach.
+# Implement a "can proceed" boolean function
+
 from utils import get_data
 grid = []
+
+class Node:
+    def __init__(self, x, y, value):
+        self.x = x
+        self.y = y
+        self.value = value
+        self.left = None
+        self.right = None
+        self.above = None
+        self.below = None
+        self.previous = None
+
 
 if __name__ == "__main__":
     stage = 0
