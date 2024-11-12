f1e631d69b7a237d53e7300ca7fb70d131a3830b
Aroush <roush.adam.h@gmail.com>
2024-11-12 15:53:34 +0000

    23_7 two failed attempts...

--- 23_17.py
+++ 23_17.py
@@ -1,27 +1,117 @@
 # Iterate down the major diagonal to find the minimum bid
-# Dijkstra-man!
-# Do Dijkstra for each cel, treating each as a little node
-# Perhaps replace the 2d grid with a grid of node objects?
-# Each having edge detection ability, a heat value, and a "previous".
-# On discovery, store a node, then check to see which nodes you can legally reach.
-# Implement a "can proceed" boolean function
+# Dijkstra-man! doesn't work. Because best path to spot n+1 != best path to n plus 1
+# Need to go back to basic bid, then rando-walk
+# Iterate down the major diagonal to find the minimum bid
+from functools import cache
 
+from listfuncs import show_2d
 from utils import get_data
 grid = []
+S = "START"
+max_bid = 0
+diag_walk = []
+walks = set()
 
 class Node:
     def __init__(self, x, y, value):
         self.x = x
         self.y = y
         self.value = value
-        self.left = None
-        self.right = None
-        self.above = None
-        self.below = None
         self.previous = None
+
+    def __repr__(self):
+        if self.previous and self.previous != S:
+            return f"({self.x},{self.y}) ${self.value} ({self.previous.x}, {self.previous.y})"
+        return f"({self.x},{self.y}) ${self.value}"
+
+def legal_proceed(path:list[Node], loc_b):
+    if len(path) <= 2: # move at most 3 in the same direction, thus a path of l=2 means no concern is warranted.
+        return True
+    xs = [v.x for v in path[:3]]+[loc_b[0]]
+    ys = [v.y for v in path[:3]]+[loc_b[1]]
+    # print(xs, ys)
+    return len(ys) > 1 and len(xs) > 1
+
+def find_path(end):
+    print("path")
+    global grid
+    x, y = end
+    hist = [grid[y][x]]
+    prev = grid[y][x].previous
+    while prev != "START":
+        x, y = prev.x, prev.y
+        hist.append(prev)
+        prev = grid[y][x].previous
+        print(hist)
+    return hist
+
+def cost_path(path: list[Node]):
+    cost = sum(l.value for l in path[1:] )
+    return cost
+
+def diagonal_bid():
+    size = len(grid)
+    x, y = 0, 0
+    path = []
+    while x < size:
+        path.append(grid[y][x])
+        if y == x:
+            x += 1
+        else:
+            y += 1
+    return path
+
+def get_neighbour(node, path_so_far):
+    x, y = node.x, node.y
+    if x > 0 and (grid[y][x-1] not in path_so_far) and legal_proceed(path_so_far, (x-1, y)):
+        yield grid[y][x-1]
+    if x < len(grid[0]) - 1 and (grid[y][x+1] not in path_so_far) and legal_proceed(path_so_far, (x+1, y)):
+        yield grid[y][x+1]
+    if y > 0 and (grid[y-1][x] not in path_so_far) and legal_proceed(path_so_far, (x, y-1)):
+        yield grid[y-1][x]
+    if y < len(grid) - 1 and (grid[y+1][x] not in path_so_far) and legal_proceed(path_so_far, (x, y+1)):
+        yield grid[y+1][x]
+
+# Go recursive to explore for better  --
+# # Fail case is if current path is already > min bid
+# # Else check L, R, and C (if it's >= 3 Cs in a row)
+# # Recursive call is  ((x,y), path_so_far)
+
+def recur_walk(cel, path_so_far: list[Node]):
+    if cost_path(path_so_far) > max_bid:
+        return max_bid, diag_walk # We've found a dead branch
+    path_so_far.append(cel)
+    if (cel.x, cel.y) == (len(grid) - 1, len(grid[0]) - 1):
+        return cost_path(path_so_far), path_so_far
+    costs, paths = [max_bid],[diag_walk]
+    for neighbour in get_neighbour(cel, path_so_far):
+        cost, path = recur_walk(neighbour, path_so_far.copy())
+        costs.append(cost)
+        paths.append(path)
+    cost = min(costs) if len(costs) else 0
+    path = paths[costs.index(cost)] if costs else []
+    if tuple(path_so_far) not in walks:
+        walks.add(tuple(path_so_far))
+        print(len(walks))
+    else:
+        print(f"Repeat path!! {path_so_far}")
+    # print(len(path_so_far))
+    return cost, path
 
 
 if __name__ == "__main__":
-    stage = 0
+    stage = 1
     data = get_data(stage, __file__, string=True)
-    grid = data.split("\n")
+    data = data.split("\n")
+
+    super_test = [[1,2,5],[2,7,2],[3,2,1]]
+    # data = super_test
+
+    grid = [[Node(i, j, int(data[j][i])) for i in range(len(data[0]))] for j in range(len(data))] # grid of nodes, so we can track la previousa
+    grid[0][0].previous = S
+
+    diag_walk = diagonal_bid()
+    max_bid = cost_path(diag_walk)
+    print(f"Max bid (diagonal): {max_bid}")
+
+    print(recur_walk(grid[0][0], []))
--- vestigials.py
+++ vestigials.py
@@ -30,6 +30,70 @@ class Cube:
         output += f"\n\nFacing side {self.facing}\n"
         return output
 
+def legal_proceed(loc_a, loc_b):
+    x, y = loc_b
+    hist = [loc_a, loc_b]
+    # get the loc b for history thing
+    prev = grid[y][x].previous
+    if prev == "START":
+        print(f"No previous for {loc_a}=>{x},{y}")
+        return True
+    hist.append((prev.x, prev.y))
+    prev2 = grid[prev.y][prev.x].previous
+    if prev2 == "START":
+        print(f"No previous for {loc_a}=>{loc_b}=>{prev.x},{prev.y}")
+        return True
+    hist.append((prev2.x, prev2.y))
+    xs = set([v[0] for v in hist])
+    ys = set(v[1] for v in hist)
+    # print(hist, xs, ys)
+    return len(ys) > 1 and len(xs) > 1
+
+def get_neighbour(x, y, visited=None):
+    if x > 0 and not grid[y][x-1].previous and legal_proceed((x-1, y),(x,y)):
+        yield grid[y][x-1]
+    if x < len(grid[0]) - 1 and not grid[y][x+1].previous and legal_proceed((x+1, y),(x,y)):
+        yield grid[y][x+1]
+    if y > 0 and not grid[y-1][x].previous and legal_proceed((x, y-1),(x,y)):
+        yield grid[y-1][x]
+    if y < len(grid) - 1 and not grid[y+1][x].previous and legal_proceed((x, y+1),(x,y)):
+        yield grid[y+1][x]
+
+
+# Attempt to create Dijstra's algorithm
+def do_dijkstra(start_loc: Node):
+    p_queue = [(start_loc, 0, "START")] # implemented as a queue of lists of the form [node, loss, prev]
+    while p_queue:
+        current_cel = p_queue.pop(0)
+        current_node = current_cel[0]
+        current_node.total_loss = current_cel[1]
+        current_node.previous = current_cel[2]
+        q_vals = [v[0] for v in p_queue]
+        for link in get_neighbour(current_node.x, current_node.y): # gotta check for legal to proceed
+            if link in q_vals:
+                temp = list(p_queue.pop(q_vals.index(link)))
+                if temp[1] > current_node.total_loss + temp[0].value:
+                    temp[1] = current_node.total_loss + temp[0].value
+                    temp = tuple(temp)
+            else:
+                temp = (link, current_node.total_loss + link.value, current_node)
+            p_queue.append(temp)
+        p_queue.sort(key=lambda x: x[1])
+        print(f"{current_node}: {p_queue}")
+
+def find_path(end):
+    print("path")
+    global grid
+    x, y = end
+    hist = [end]
+    prev = grid[y][x].previous
+    while prev != "START":
+        hist.append((prev.x, prev.y))
+        x, y = prev.x, prev.y
+        prev = grid[y][x].previous
+        print(hist)
+    return hist
+
 if __name__ == '__main__':
     world = Cube(2)
     for i in range(1,7):
